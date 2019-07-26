e32b73f8837541243c452181736cda0a016dfa12
Orenchuck <50010695+Orenchuck@users.noreply.github.com>
2019-07-26 15:48:15 +0300

    Add files via upload
    try to add decor

--- decor/macaron.jsx
+++ decor/macaron.jsx
@@ -5,11 +5,13 @@ export default class Macaron_el extends React.Component {
     constructor (props) {
         super (props);
         this.state = {
-            elDecor: '',
+            elDecor: {},
             showPage: true,
         }
         this.onClick = this.onClick.bind(this);
         this.onChange = this.onChange.bind(this);
+        this.whatPicture = this.whatPicture.bind(this);
+        this.d = this.d.bind(this);
     }
 
     onClick () {
@@ -20,22 +22,38 @@ export default class Macaron_el extends React.Component {
         this.setState ({elDecor: `${event.target.value}`});
     }
 
+    whatPicture (event) {
+        this.setState({elDecor:{src: event.target.src,
+        x: 0, y: 0}});
+        console.log(this.state.elDecor);
+    }
+
+    d(e) {
+        this.whatPicture(e);
+        this.props.add(this.state.elDecor);
+    }
+
     render () {
         return (
             <div>
                 {this.state.showPage ? (<div>
-                <img src='http://www.pngall.com/wp-content/uploads/2/Macaron-PNG-Free-Image.png'
+                    <img src='http://www.pngall.com/wp-content/uploads/2/Macaron-PNG-Free-Image.png'
                  style={{width: '10vh'}}
-                onClick = {() => {this.props.add(this.state.elDecor)}}></img>
+                // onClick = {() => {this.props.add(this.state.elDecor)}}
+                onClick = {this.d}
+                >
+                </img>
                 <img src='https://www.stickpng.com/assets/thumbs/586ab2361fdce414493f510c.png'
                  style={{width: '10vh'}}
-                onClick = {() => {this.props.add(this.state.elDecor)}}></img>
+                // onClick = {() => {this.props.add(this.state.elDecor)}}
+                onClick = {this.d}
+                ></img>
 
                 <img src='https://library.kissclipart.com/20180914/zie/kissclipart-macaron-transparent-background-clipart-french-cuis-e46c9087ba4c9a1b.png'
                  style={{width: '10vh'}}
                 onClick = {() => {this.props.add(this.state.elDecor)}}></img>
                 <br/>
-                <label> Если хотите добавить свое подготовленное изображение
+                {/* <label> Если хотите добавить свое подготовленное изображение
                      - вставьте на него ссылку
                     <input type="text" onChange={this.onChange}></input></label>
                     <br/>
@@ -47,7 +65,7 @@ export default class Macaron_el extends React.Component {
                      в приложении &nbsp;
                      <a target="_blank" style={{textDecoration: 'none'}} 
                      href="https://www.imgonline.com.ua/replace-white-background-with-transparent.php">
-                       IMGonline.com.ua</a> </div>
+                       IMGonline.com.ua</a> </div> */}
                 <button onClick ={this.onClick}>Назад</button> 
                 </div>) : <CakeDecorating />}                
                 
