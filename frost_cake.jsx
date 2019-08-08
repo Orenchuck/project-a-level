68b755fcaa78978e348818229b74049a30628432
anv6264 <49003246+anv6264@users.noreply.github.com>
2019-08-08 19:58:05 +0300

    Update frost_cake.jsx

--- frost_cake.jsx
+++ frost_cake.jsx
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-export default class FrostCake extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            color: '#520599',
-        }
-        this.ChangeColor = this.ChangeColor.bind(this);
-    }
-    ChangeColor() {
-        this.setState({ color: event.target.value })
-    }
-    render() {
-        return (
-            <div id='options'>
-                <div className='inner'>
-                    <span className='headline'>Выберите цвет</span> 
-                    <input  className='color'
-                            type='color' 
-                            value={this.state.color} 
-                            onChange={this.ChangeColor}>
-                    </input>
-                </div>
-                <label>
-                    <p className='headline'>Выберите покрытие торта:</p> 
-                    <select value={this.state.value} onChange={this.handleChange}>
-                        <option className='cream' value="cream">Крем</option>
-                        <option className="fondant" value="fondant">Мастика</option>
-                        <option className="icing" value="icing">Зеркальная глазурь</option>
-                        <option className="velour" value="velour">Велюр</option>
-                    </select>
-                </label>
-                <button onClick={() => this.props.StepFrostCake(this.state.color,
-                                      }>Ок</button>
-            </div>
-        )
-    }
-
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -89,4 +46,4 @@ export default class FrostCake extends React.Component {
         )
     }
 
->>>>>>> 023f5b1f92d7a487fa2f463c8303d44
+
